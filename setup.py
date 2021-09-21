1b5d844b3d9a252cabf85a316ea0974019da2fea
Gonzalo <33635010+gon-uri@users.noreply.github.com>
2021-09-21 13:22:51 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bird_detection",
+    version="0.0.1",
+    author="Gonzalo Uribarri",
+    description="Algorithm for bird detection through vocalizations",
+    long_description_content_type="text/markdown",
+    url="https://github.com/gon-uri/bird_detection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.7',
+)
