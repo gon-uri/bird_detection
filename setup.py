b7778fba92b6597301e98d079235deefceb94529
Gonzalo <33635010+gon-uri@users.noreply.github.com>
2021-09-23 12:09:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bird_detection",
+    name="birdect",
     version="0.0.1",
     author="Gonzalo Uribarri",
     description="Machine learning algorithm to identify the presence and location of birds.",
